f7d98f175d6e1b94b9533fd605755736ef4bc114
KhrolenkoEkaterina <128682248+KhrolenkoEkaterina@users.noreply.github.com>
2023-11-06 17:08:29 +1000

    Add files via upload

---
+++ mapping23/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataList from "./DataList";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Список данных:</h1>
+      <DataList />
+    </div>
+  );
+};
+
+export default App;
---
+++ mapping23/src/data.js
@@ -0,0 +1,10 @@
+const data = [
+  { name: "Екатерина", age: 19, height: 172 },
+  { name: "Елизавета", age: 25, height: 170 },
+  { name: "Валерия", age: 27, height: 160 },
+  { name: "Даниил", age: 34, height: 169 },
+  { name: "Никита", age: 28, height: 180 },
+  { name: "Андрей", age: 29, height: 190 }
+];
+
+export default data;
